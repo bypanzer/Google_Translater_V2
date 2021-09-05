4f50433373ab2474fe245b3c22e4e6e51b7b37dc
bypanzer <89009367+bypanzer@users.noreply.github.com>
2021-09-05 20:52:55 +0400

    Create users.py

---
+++ plugins/users.py
@@ -0,0 +1,13 @@
+import os
+from pyrogram import Client ,filters
+from helper.database import getid
+ADMIN = int(os.environ.get("ADMIN", 923943045))
+
+
+@Client.on_message(filters.private & filters.user(ADMIN) & filters.command(["users"]))
+async def users(bot, message):
+ if (message.reply_to_message):
+   ms = await message.reply_text("Bütün İD-lər yaddaşdan alınır ...")
+   ids = getid()
+   tot = len(ids)
+   await ms.edit(f"Bütün istifadəçilərin sayı: {tot}")
